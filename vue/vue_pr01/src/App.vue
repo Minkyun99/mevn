2eedc44a81a92aedaa7474a791663ea3c96592e4
Minkyun99 <kyung865821@gmail.com>
2022-12-23 15:24:43 +0900

    20221223

--- vue/vue_pr01/src/App.vue
+++ vue/vue_pr01/src/App.vue
@@ -1,7 +1,7 @@
 <template>
  <div>
   <h1>안녕하세요.{{ vname }}반갑습니다.{{ age }}살 입니까?</h1>
-  <button v-on:click="">버튼</button>
+  <button @click="func('안녕 팝업이야.')">버튼</button>
  </div>
 </template>
 
@@ -12,6 +12,11 @@ export default {
     return{
       vname: '홍길동',
       age : 20
+    }
+  },
+  methods:{
+    func : function(text){
+      alert(text)
     }
   }
 }
