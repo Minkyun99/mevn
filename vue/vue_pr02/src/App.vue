998a4d2698044fbb749bb1a2e25b81c1c8fe1242
Minkyun99 <kyung865821@gmail.com>
2022-12-27 09:14:52 +0900

    20221227

---
+++ vue/vue_pr02/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>원하는 구구단을 입력해주세요.</h1>
+  <input type="text" v-model="value" v-on:keyup="gugudan(value)">
+  <h1>{{ value }}</h1>
+</template>
+
+<script>
+  export default {
+    gugudan(){
+      return{
+        value : '',
+      }
+    },
+  
+  methods:{
+    gugudan(text){
+      Number(text)
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
