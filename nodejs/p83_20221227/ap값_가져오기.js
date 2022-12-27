35e69c4e3b01e078791f5b69de12e543ff7f5db3
Minkyun99 <kyung865821@gmail.com>
2022-12-27 15:57:41 +0900

    20221227

--- nodejs/p83_20221227/ap값_가져오기.js
+++ nodejs/p83_20221227/ap값_가져오기.js
@@ -3,3 +3,6 @@ document.addEventListener('DOMContentLoaded',()=>{
 })
 
 https://api.upbit.com/v1/ticker?markets=KRW-BTC
+
+
+https://www.tmon.co.kr/deal/15031575718?coupon_srl=3041958&utm_source=naver&utm_medium=affiliate&utm_term=72093_1008&utm_content=&utm_campaign=META_%EB%84%A4%EC%9D%B4%EB%B2%84%EC%A7%80%EC%8B%9D%EC%87%BC%ED%95%91
