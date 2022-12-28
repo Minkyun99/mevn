ea5041731c2ea20f650448affd13c8df2d8a831c
Minkyun99 <alsrud7412@naver.com>
2022-12-28 17:47:58 +0900

    20221228

--- vue/vue_pr04/src/router/index.js
+++ vue/vue_pr04/src/router/index.js
@@ -7,6 +7,7 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+
   {
     path: '/ex',
     name: 'ex',
@@ -16,26 +17,44 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "ex" */ '../views/HomeView.vue')
   },
+
   {
     path: '/ex1',
     name: 'ex',
-
+    // route level code-splitting
+    // this generates a separate chunk (ex.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "ex" */ '../views/ex1.vue')
   },
+
   {
     path: '/ex2',
     name: 'ex',
+    // route level code-splitting
+    // this generates a separate chunk (ex.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "ex" */ '../views/ex2.vue')
   },
+
   {
     path: '/ex3',
     name: 'ex',
+    // route level code-splitting
+    // this generates a separate chunk (ex.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "ex" */ '../views/ex3.vue')
   },
+
   {
     path: '/ex4',
     name: 'ex',
-    component: () => import(/* webpackChunkName: "ex" */ '../views/ex4.vue')
+    // route level code-splitting
+    // this generates a separate chunk (ex.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "ex" , webpackPrefetch:true */ '../views/ex4.vue'
+      )
   }
 ]
 
--- vue/vue_pr04/src/views/ex4.vue
+++ vue/vue_pr04/src/views/ex4.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <button @click="status = !status">image1</button>
+    <button
+      v-for="p in pages"
+      @click="page = p"
+      :key="p"
+      :class="{ active: page == p }"
+    >
+      {{ p }}
+    </button>
+    <div v-for="(g, i) in pages" :key="i">
+      <div v-if="g == page">{{ g }}</div>
+      <img
+        v-if="g == page"
+        :src="'../../모코코' + (i + 1) + '.png'"
+        alt="사진없음"
+        srcset=""
+      />
+    </div>
   </div>
 </template>
 
@@ -10,24 +26,32 @@ export default {
   data() {
     return {
       page: 'page1',
-      pages: ['page1', 'page2', 'page3', 'page4']
+      pages: ['page1', 'page2', 'page3', 'page4', 'page5']
     }
   },
-
-  methods: {
-    image1: function () {
-      if ((this.status = true)) {
-      }
-    }
-  }
+  methods: {}
 }
 </script>
 
 <style>
 button {
-  width: '100px';
-  height: '50px';
+  font-size: '20px';
+  width: '150px';
+  height: '80px';
   border-radius: 20px;
   border: 3px double black;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button:hover {
+  background-color: darkcyan;
+}
+
+button .active {
+}
+
+img {
+  width: 50%;
 }
 </style>
