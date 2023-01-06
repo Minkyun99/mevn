a60f241bb5f01393383b3d191ced938dea5d4c8c
Minkyun99 <kyung865821@gmail.com>
2023-01-06 13:58:42 +0900

    20230106

---
+++ vue/vue_pr06/src/views/bindSelector.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <select name="" id="" v-model="selData"></select>
+    <option value="" v-for="(data, i) in op" :key="i">{{ data }}</option>
+  </div>
+  <input type="text" v-on:keyup.enter="write()" v-model="write_info" />
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      selData: '고르세요.',
+      op: [],
+      write_info: ''
+    }
+  },
+  methods: {
+    write: function () {
+      this.op.push(this.write_info)
+    }
+  }
+}
+</script>
+
+<style></style>
